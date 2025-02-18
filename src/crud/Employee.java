2b82f7b1267090e44ae863c84ca8a2e40ad45b3c
MohammedMustafa1096 <mohammedmustafa1096@gmail.com>
2025-02-18 19:45:16 +0530

    adding JDBC-CRUD project

---
+++ src/crud/Employee.java
@@ -0,0 +1,51 @@
+package crud;
+
+public class Employee {
+
+	private int empid;
+	private String empname;
+	private String designation;
+	private int review;
+	private int salary;
+
+	public int getEmpid() {
+		return empid;
+	}
+
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+
+	public String getEmpname() {
+		return empname;
+	}
+
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public int getReview() {
+		return review;
+	}
+
+	public void setReview(int review) {
+		this.review = review;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+}
